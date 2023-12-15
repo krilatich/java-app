34e3afe8dcd1a60079d40a4c1e94c7c34289b6b3
Dmitry Vasiliev <106861958+krilatich@users.noreply.github.com>
2023-12-15 15:33:45 +0700

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -18,7 +18,7 @@ public class App {
     public void init()
     {
         Logger log = LoggerFactory.getLogger(App.class);
-        log.info("Java app started");
+        log.info("Java app started 15.12");
     }
 
     public String getStatus() {
